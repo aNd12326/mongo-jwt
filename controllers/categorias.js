c7e32077e20fc00ca0c1e3840e0ddf58a2409f6d
AndersonVT <andersonventosilla0@gmail.com>
2022-11-03 20:00:25 -0500

    173 - crud rutas y productos

--- controllers/categorias.js
+++ controllers/categorias.js
@@ -1,6 +1,35 @@
 const { response } = require("express");
 const { Categoria } = require("../models");
 
+// obtenerCategorias - paginado - total - populate
+// populate =>  para la relacion para que aparezca toda la info del user
+
+const obtenerCategorias = async (req, res = response) => {
+  const { limite = 5, desde = 0 } = req.query;
+  const query = { estado: true };
+
+  const [total, categorias] = await Promise.all([
+    Categoria.countDocuments(query),
+    Categoria.find(query)
+      .populate("usuario", "nombre")
+      .skip(Number(desde))
+      .limit(Number(limite)),
+  ]);
+
+  res.json({
+    total,
+    categorias,
+  });
+};
+
+// obtenerCategoria - populate {}
+const obtenerCategoria = async (req, res = response) => {
+  const { id } = req.params;
+  const categoria = await Categoria.findById(id).populate("usuario", "nombre");
+
+  res.json(categoria);
+};
+
 const crearCategoria = async (req, res = response) => {
   const nombre = req.body.nombre.toUpperCase();
 
@@ -26,4 +55,36 @@ const crearCategoria = async (req, res = response) => {
   res.status(201).json(categoria);
 };
 
-module.exports = { crearCategoria };
+// actualizarCategoria
+const actualizarCategoria = async (req, res = response) => {
+  const { id } = req.params;
+  const { estado, usuario, ...data } = req.body;
+
+  data.nombre = data.nombre.toUpperCase();
+  data.usuario = req.usuario._id;
+
+  const categoria = await Categoria.findByIdAndUpdate(id, data, { new: true });
+
+  res.json(categoria);
+};
+
+// borrarCategoria - estado a false
+const borrarCategoria = async (req, res = response) => {
+  const { id } = req.params;
+
+  const categoriaBorrada = await Categoria.findByIdAndUpdate(
+    id,
+    { estado: false },
+    { new: true }
+  );
+
+  res.json(categoriaBorrada);
+};
+
+module.exports = {
+  crearCategoria,
+  obtenerCategorias,
+  actualizarCategoria,
+  obtenerCategoria,
+  borrarCategoria,
+};
